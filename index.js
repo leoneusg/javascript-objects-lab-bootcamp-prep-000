6a8bc8d1b8cee164aec59592ce3402bdf779986f
Kendal Gates <leoneusg@yahoo.com>
2017-09-28 21:41:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-var recipes = { key: 'value'}
-function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
-}
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({},object, { [key]: value })
-}
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign(object, { [key]:  value})
-  return newObject
-}
-function deleteFromObjectByKey(object, key) {
-  var object = [key]
-  var newObject = Object.assign(object, [key])
-  delete newObject[key]
-  return object
-}
-function destructivelyDeleteFromObjectByKey(object, key) {
-  var object = [key]
-  var newObject = Object.assign(object, [key])
-  return object
-}
-function destructivelyDeleteFromObjectByKey(object, key) {
-   delete object[key]
+var recipes = {}
 
-   return object
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+
+  return object
 }
